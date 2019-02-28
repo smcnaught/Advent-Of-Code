e7bc9c25abb153d33c6f615ba48583e6b1a08624
Stephanie McNaught <StephanieM@DHICorp.com>
2019-02-28 09:48:35 -0700

    Adding 2015 Day 1

---
+++ 2015/01 Not Quite Lisp/Part 1.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString('utf-8');
+
+let count = 0;
+
+for(let i = 0; i < input.length; i++) {
+	if (input.charAt(i) === '(') count++;
+	else if (input.charAt(i) === ')') count--;
+}
+
+console.log(`Part One Answer: ${count}`)
---
+++ 2015/01 Not Quite Lisp/Part 2.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString('utf-8');
+
+let count = 0;
+let num = 0;
+let answeredPartTwo = false; 
+
+for(let i = 0; i < input.length; i++) {
+	if (input.charAt(i) === '(')
+	{
+		num++;
+		count++;
+	} 
+	else if (input.charAt(i) === ')')
+	{
+		num++;
+		count--;
+	}
+
+	if (count === -1 && !answeredPartTwo) {
+		answeredPartTwo = true;
+		console.log(`Part Two Answer: ${num}`);
+	}
+}
