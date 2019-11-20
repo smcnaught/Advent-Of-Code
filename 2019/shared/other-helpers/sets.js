9713f39530ef074517e21c0f5d5fd64cdf6e6cfd
smcnaught <stephaniemcnaught@gmail.com>
2019-11-19 19:57:38 -0700

    some notes on sets

---
+++ 2019/shared/other-helpers/sets.js
@@ -0,0 +1,62 @@
+/**
+ * Sets automatically filter duplicates.
+ */
+
+/**
+ * *** 3 different ways to create a new set ***
+ */
+// let set = new Set(['banana', 'apple', 'banana', 'pear', 'orange', 'pear']); // initialize with an array
+// let set = new Set('aabbcc'); // initialize with a string
+// let set = new Set(); // Without initializing
+
+/**
+ * *** Add elements to your set ***
+ */
+// set.add('hippo');
+// let newElements = ['dog', 'cat', 'bird'];
+// newElements.forEach(newEl => set.add(newEl));
+
+
+/**
+ * *** Remove elements from your set ***
+ */
+// set.delete('hippo');
+
+
+/**
+ * *** Get your set ***
+ */
+// console.log(`Get your set: `, set);
+
+
+/**
+ * *** Get your set as a string ***
+ */
+// console.log('Get your set as a string: ', ...set);
+
+
+/**
+ * *** Check length / size ***
+ */
+// console.log(`Get the size of your set: ${set.size}`);
+
+
+/**
+ * *** Check if your set contains an element ***
+ */
+// console.log(`Check if your set contains an element. Contains 'bird': ${set.has('bird')}`)
+
+
+/**
+ * *** Loop through a set ***
+ */
+// for (const element of set) {
+//   console.log(element);
+// }
+// set.forEach(i => console.log(i));
+
+
+/**
+ * *** Clear your set ***
+ */
+// set.clear();
