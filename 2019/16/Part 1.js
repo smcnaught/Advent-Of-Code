1317d7ee782912441f6b63e2c659afd69d3ec3ca
smcnaught <stephaniemcnaught@gmail.com>
2020-05-27 00:50:33 -0600

    2019 Day 16

--- 2019/16/Part 1.js
+++ 2019/16/Part 1.js
@@ -1,3 +1,65 @@
 let fs = require('fs');
-let source = fs.readFileSync('./input.txt').toString('utf-8');
-// let source = fs.readFileSync('./sampleInput.txt').toString('utf-8');
+const signal = fs.readFileSync('./input.txt').toString('utf-8').split('').map(Number);
+const basePattern = [0, 1, 0, -1];
+let allPhases = [];
+
+function setPattern(rowNum, inputLength) {
+  let pattern = [];
+
+  while (pattern.length < inputLength)
+  {
+    for (let i = 0; i < basePattern.length; i++)
+    {
+      for (let j = 0; j <= rowNum; j++)
+      {
+        pattern.push(basePattern[i]);
+        if (pattern.length === inputLength) break;
+      }
+
+      if (pattern.length === inputLength) break;
+    }
+  }
+
+  // remove first item in pattern
+  pattern.shift();
+  return pattern;
+}
+
+
+function getNewNumber(row, pattern) {
+  let newNum = null;
+
+  for (let i = 0; i < row.length; i++)
+  {
+    newNum += row[i] * pattern[i];
+  }
+
+  if (isNaN(newNum)) console.log('new number did not get set!');
+  
+  // Then, only the ones digit is kept: 38 becomes 8, -17 becomes 7, and so on.
+  return +newNum.toString().split('').pop();
+}
+
+function fft(phases, inputList) {
+  let oldList = JSON.parse(JSON.stringify(inputList));
+  let newList = [];
+  
+  for (let i = 0; i < phases; i++) {
+    
+    for (let k = 0; k < oldList.length; k++) {
+      let pattern = setPattern(k, oldList.length + 1);
+
+      let newNum = getNewNumber(oldList, pattern);
+      newList.push(newNum)
+    }
+
+    allPhases.push(newList.join(''));
+
+    // prepare for next phase.
+    oldList = JSON.parse(JSON.stringify(newList));
+    newList = [];
+  }
+}
+
+fft(100, signal);
+console.log(`Part One Answer: ${allPhases[allPhases.length - 1].toString().substring(0, 8)}`);
--- 2019/16/Part 2.js
+++ 2019/16/Part 2.js
@@ -1,3 +1,21 @@
 let fs = require('fs');
-let source = fs.readFileSync('./input.txt').toString('utf-8');
-// let source = fs.readFileSync('./sampleInput.txt').toString('utf-8');
+let input = fs.readFileSync('./input.txt').toString();
+let signal = input.split("").map(Number);
+const messageOffset = +input.slice(0, 7);
+
+// Repeat signal 10000 times
+for (let i = 1; i < 10000; i++) {
+    for (let n = 0; n < input.length; n++) signal.push(signal[n]);
+}
+
+signal = signal.slice(messageOffset);
+
+// run 100 phases of fft
+for (let phase = 1; phase <= 100; phase++) {
+    for (let i = signal.length - 1; i >= 0; i--) {
+        signal[i] = Math.abs((signal[i + 1] || 0) + signal[i]) % 10;
+    }
+}
+
+const eightDigitMessage = signal.slice(0, 8).join("");
+console.log(`Part Two Answer: ${eightDigitMessage}`);
