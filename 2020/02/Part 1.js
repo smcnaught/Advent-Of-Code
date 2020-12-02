e767a761ff9f18e5935ecd4e6db889f0b2532775
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-02 13:07:10 -0700

    2020 Day 2

--- 2020/02/Part 1.js
+++ 2020/02/Part 1.js
@@ -1,3 +1,15 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let passwords = new formatter.Formatter(__dirname).getArrayOfStringsByLine();
+let valid = 0;
+
+passwords.forEach(p => {
+  const min = p.substring(0, p.indexOf("-"));
+  const max = p.substring(p.indexOf("-") + 1, p.indexOf(" "));
+  const searchingFor = p.substring(p.indexOf(" ") + 1, p.indexOf(":"));
+  const str = p.substring(p.indexOf(":") + 1, p.length);
+
+  const count = str.split(searchingFor).length -1;
+  if (count >= min && count <= max) valid++;
+})
+
+console.log(`Part One Answer: ${valid}`);
--- 2020/02/Part 2.js
+++ 2020/02/Part 2.js
@@ -1,3 +1,17 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let passwords = new formatter.Formatter(__dirname).getArrayOfStringsByLine();
+let valid = 0;
+
+passwords.forEach(p => {
+  const posA = +(p.substring(0, p.indexOf("-")));
+  const posB = +(p.substring(p.indexOf("-") + 1, p.indexOf(" ")));
+
+  const searchingFor = p.substring(p.indexOf(" ") + 1, p.indexOf(":"));
+  const str = p.substring(p.indexOf(":") + 1, p.length);
+
+  if ((str.charAt(posA) === searchingFor && str.charAt(posB) != searchingFor) || 
+    (str.charAt(posB) === searchingFor && str.charAt(posA) != searchingFor))
+    valid ++;
+})
+
+console.log(`Part Two Answer: ${valid}`);
