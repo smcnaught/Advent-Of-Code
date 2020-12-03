0d9a85c3c53e556331b4ff9af8b5bbd9424450f9
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-03 11:19:16 -0700

    2020 Day 3

--- 2020/03/Part 1.js
+++ 2020/03/Part 1.js
@@ -1,3 +1,19 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).get2DArrayOfStrings('');
+let currentRight = treeCount = 0;
+
+for (let i = 0; i < data.length; i++) {
+  let repeat = JSON.parse(JSON.stringify(data[i]));
+  for (let j = 0; j < 75; j++) data[i].push(...repeat);
+}
+
+for (let i = 0; i < data.length; i++) {
+  if (i === 0) currentRight += 3;
+  else {
+    if (data[i][currentRight] === "#") treeCount++;
+    // else if (data[i][currentRight] !== ".") console.log(data[i][currentRight]) // if you get this, you don't have enough repeats
+    currentRight += 3;
+  }
+}
+
+console.log(`Part One Answer: ${treeCount}`);
--- 2020/03/Part 2.js
+++ 2020/03/Part 2.js
@@ -1,3 +1,27 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).get2DArrayOfStrings('');
+let allTrees = [];
+
+for (let i = 0; i < data.length; i++) {
+  let repeat = JSON.parse(JSON.stringify(data[i]));
+  for (let j = 0; j < 75; j++) data[i].push(...repeat);
+}
+
+function getSlope(r, d) {
+  let treeCount = currentRight = 0;
+
+  for (let i = 0; i < data.length; i += d) {
+    if (i === 0) currentRight += r;
+    else {
+      if (data[i][currentRight] === "#") treeCount++;
+      // else if (data[i][currentRight] !== ".") console.log(data[i][currentRight]) // if you get this, you don't have enough repeats
+      currentRight += r;
+    }
+  }
+
+  return treeCount;
+}
+
+let slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+slopes.forEach(s => allTrees.push(getSlope(s[0], s[1])));
+console.log(`Part Two Answer: ${allTrees.reduce((prev, cur) => prev * cur, 1)}`);
