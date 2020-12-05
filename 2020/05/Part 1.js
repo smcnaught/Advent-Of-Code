8c002c02f47b5bab4a142eb1cde216f1cdc23ab7
smcnaught <stephaniemcnaught@gmail.com>
2020-12-04 23:57:48 -0700

    2020 Day 5

--- 2020/05/Part 1.js
+++ 2020/05/Part 1.js
@@ -1,3 +1,32 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).get2DArrayOfStrings('');
+let uniqueSeatIDs = [];
+
+data.forEach(d => {
+  let rowMin = 0;
+  let rowMax = 127;
+  let row;
+
+  for (let i = 0; i < 6; i++) {
+    if (d[i] === 'F') rowMax = Math.floor(((rowMax - rowMin) / 2) + rowMin);
+    else if (d[i] === 'B') rowMin = Math.round(((rowMax - rowMin) / 2) + rowMin);
+  }
+
+  row = d[6] === 'F' ? rowMin : rowMax;
+
+  let columnMin = 0;
+  let columnMax = 7;
+  let column;
+
+  for (let j = 7; j < 9; j++) {
+    if (d[j] === 'L') columnMax = Math.floor(((columnMax - columnMin) / 2) + columnMin);
+    else if (d[j] === 'R') columnMin = Math.round(((columnMax - columnMin) / 2) + columnMin);
+  }
+
+  column = d[9] === 'L' ? columnMin : columnMax;
+
+  const uniqueSeatID = (row * 8) + column;
+  uniqueSeatIDs.push(uniqueSeatID);
+})
+
+console.log(`Part One Answer: ${uniqueSeatIDs.sort((a, b) => b - a)[0]}`);
--- 2020/05/Part 2.js
+++ 2020/05/Part 2.js
@@ -1,3 +1,38 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).get2DArrayOfStrings('');
+let allSeats = [];
+
+data.forEach(d => {
+  let rowMin = 0;
+  let rowMax = 127;
+  let row;
+
+  for (let i = 0; i < 6; i++) {
+    if (d[i] === 'F') rowMax = Math.floor(((rowMax - rowMin) / 2) + rowMin);
+    else if (d[i] === 'B') rowMin = Math.round(((rowMax - rowMin) / 2) + rowMin);
+  }
+
+  row = d[6] === 'F' ? rowMin : rowMax;
+
+  let columnMin = 0;
+  let columnMax = 7;
+  let column;
+
+  for (let j = 7; j < 9; j++) {
+    if (d[j] === 'L') columnMax = Math.floor(((columnMax - columnMin) / 2) + columnMin);
+    else if (d[j] === 'R') columnMin = Math.round(((columnMax - columnMin) / 2) + columnMin);
+  }
+
+  column = d[9] === 'L' ? columnMin : columnMax;
+
+  if (allSeats[row]) allSeats[row].push(column);
+  else allSeats[row] = [column];
+})
+
+for (let i = 0; i < allSeats.length; i++) {
+  if (allSeats[i] && allSeats[i].length !== 8 && allSeats[i].length > 2) {
+    let missing = 28 - (allSeats[i].reduce((a, b) => a + b, 0));
+    const uniqueSeatID = (i * 8) + missing;
+    console.log(`Part Two Answer: ${uniqueSeatID}`);
+  }
+}
