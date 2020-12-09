3f2e03e90fcf4f0df577fc61b58472d3d489a0ce
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-09 16:42:28 -0700

    2020 Day 7

--- 2020/07/Part 1.js
+++ 2020/07/Part 1.js
@@ -1,3 +1,27 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getRaw()
+  .replace(/contain no other bags.|\r|/g, '')
+  .replace(/ contain /g, ",")
+  .replace(/bags/g, 'bag')
+  .split('\n')
+  .map(e => e.split(','))
+
+let queue = ["shiny gold"];
+let bagsContainingGold = [];
+
+while (queue.length != 0) {
+  data.forEach(bags => {
+    bags.forEach(b => {
+      queue.forEach(q => {
+        if (b.includes(q) && 
+            bags[0] !== "shiny gold bag" && 
+            !bagsContainingGold.includes(bags[0])) {
+          bagsContainingGold.push(bags[0])
+          queue.push(bags[0]);
+        }
+      })
+    })
+  })
+  queue.shift();
+}
+console.log(`Part One Answer: ${bagsContainingGold.length}`);
--- 2020/07/Part 2.js
+++ 2020/07/Part 2.js
@@ -1,3 +1,36 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+const lines = new formatter.Formatter(__dirname).getRaw()
+  .replace(/ contain /g, ",")
+  .replace(/\.|bags|bag/g, '')
+  .split('\n')
+  .map(e => e.split(','))
+
+const map = new Map();
+
+let setMap = function () {
+  lines.forEach(l => {
+    const bag = l[0].trim();
+  
+    for (let i = 1; i < l.length; i++) {
+      l[i] = l[i].trim();
+      const count = (l[i].substring(0, l[i].indexOf(" ")));
+      const bags = (l[i].substring(l[i].indexOf(" ") + 1, l[i].length));
+  
+      if (!map.has(bag)) map.set(bag, []);
+      const info = { count: count && count !== 'no' ? count : 0, bag: bags.trim() };
+
+
+      map.set(bag, [...map.get(bag), info]);
+    }
+  })
+}();
+
+let recurCountBags = (toFind) => {
+  if (toFind.count == 0) return 0;
+  const bagsWithin = map.get(toFind.bag);
+  let count = 1;
+  for (const bag of bagsWithin) count += bag.count * recurCountBags(bag);
+  return count;
+}
+
+console.log(`Part Two Answer: ${recurCountBags({ count: 1, bag: 'shiny gold' }) - 1}`);
