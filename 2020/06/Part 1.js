75bf9fab08aec430872d17a1608efdcea4c86470
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-09 16:50:48 -0700

    2020 Day 6

--- 2020/06/Part 1.js
+++ 2020/06/Part 1.js
@@ -1,3 +1,6 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getArrayByLineBreaks();
+let count = 0;
+const removeDuplicateCharacters = (str) => str.split('').filter((item, pos, self) => self.indexOf(item) == pos).join('');
+data.forEach(str => count += removeDuplicateCharacters(str).length);
+console.log(`Part One Answer: ${count}`);
--- 2020/06/Part 2.js
+++ 2020/06/Part 2.js
@@ -1,3 +1,28 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getArrayOfStringsByLine();
+let yes = [];
+data.push("");
+let allLettersInGroup = [];
+let membersOfGroup = 0;
+
+for (let i = 0; i < data.length; i++) {
+  let str = data[i];
+  str.split('').forEach(s => allLettersInGroup.push(s));
+  if (str !== "") membersOfGroup++;
+  else {
+    allLettersInGroup.sort();
+    let counts = {};
+    allLettersInGroup.forEach((x) => counts[x] = (counts[x] || 0) + 1);
+
+    let groupYesCount = 0;
+    Object.entries(counts).forEach(([key, value]) => {
+      if (value === membersOfGroup) groupYesCount++;
+    });
+
+    yes.push(groupYesCount);
+
+    allLettersInGroup = [];
+    membersOfGroup = 0;
+  }
+}
+console.log(`Part Two Answer: ${yes.reduce((a, b) => a + b, 0)}`);
