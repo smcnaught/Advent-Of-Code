7043ef644b8e0bd56526dd3d03db8ad5547e2668
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-09 19:42:10 -0700

    2020 Day 9

--- 2020/09/Part 1.js
+++ 2020/09/Part 1.js
@@ -1,3 +1,25 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getArrayOfNumbersByLine();
+let preambleLength = 25;
+
+for (let i = preambleLength; i < data.length; i++) {
+  let preamble = [];
+  for (let j = 1; j <= preambleLength; j++) preamble.push(data[i - j])
+  const allSumsForPreamble = getAllSums(preamble);
+  if (!allSumsForPreamble.includes(data[i])) console.log(`Part One Answer: ${data[i]}`);
+}
+
+function getAllSums(preamble) {
+  let availableSums = [];
+
+  for (let i = 0; i < preamble.length; i++) {
+    for (let j = 0; j < preamble.length; j++) {
+      if (i !== j) {
+        sum = preamble[i] + preamble[j];
+        availableSums.push(sum);
+      }
+    }
+  }
+
+  return availableSums;
+}
--- 2020/09/Part 2.js
+++ 2020/09/Part 2.js
@@ -1,3 +1,16 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getArrayOfNumbersByLine();
+const p1 = 675280050;
+
+for (let i = 0; i < data.length; i++) {
+  let current = data[i];
+  let prev = [current];
+  for (let j = i + 1; j < data.length; j++) {
+    current += data[j];
+    prev.push(data[j])
+    if (current === p1) {
+      prev.sort((a, b) => a - b);
+      console.log(`Part Two Answer: ${prev[0] + prev[prev.length - 1]}`);
+    }
+  }
+}
