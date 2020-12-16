45692a31203a102c44f5828dd90e81e7c6f97df6
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-15 22:29:38 -0700

    2020 Day 13

--- 2020/13/Part 2.js
+++ 2020/13/Part 2.js
@@ -1,3 +1,23 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getArrayOfStringsByLine()[1].split(',');
+let busInfo = [];
+
+for (let i = 0; i < data.length; i++) {
+  if (!isNaN(data[i])) busInfo.push([+data[i], i])
+}
+
+let earliestTime = 0;
+let multiplier = 1;
+for (let i = 0; i < busInfo.length; i++) {
+  let foundEarliest = false;
+  [bus, order] = [busInfo[i][0], busInfo[i][1]]
+
+  while (!foundEarliest) {
+    if ((earliestTime + order) % bus === 0) {
+      foundEarliest = true;
+      multiplier *= bus;
+    }
+    else earliestTime += multiplier;
+  }
+}
+console.log(`Part Two Answer: ${earliestTime}`);
