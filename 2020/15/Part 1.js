6a17f16ffe1aafc9990d21d5d444aef8b0dae936
Stephanie McNaught <StephanieM@DHICorp.com>
2020-12-17 16:55:03 -0700

    2020 Day 15

--- 2020/15/Part 1.js
+++ 2020/15/Part 1.js
@@ -1,3 +1,34 @@
 const formatter = require('../../shared/formatting/format-puzzle-input');
-let data = new formatter.Formatter(__dirname).getRaw(); // change method call to what you need
-console.log(data);
+let data = new formatter.Formatter(__dirname).getArrayOfNumbers(',');
+let info = {}; // number: { turnBefore: null, turnLastSaid: 4, timesSpoken: 1 };
+let turnCount = 0;
+let timesToRun = 2020;
+
+for (let i = 0; i < data.length; i++) {
+  turnCount++;
+  info[data[i]] = { turnBefore: null, turnLastSaid: i + 1, timesSpoken: 1};
+
+  if (i === data.length - 1) run2020Turns(data[i]);
+}
+
+function run2020Turns(lastSpoken) {
+  while (turnCount < timesToRun) {
+    let nextSpoken = info[lastSpoken].timesSpoken > 1 ? (turnCount - info[lastSpoken].turnBefore) : 0;
+    turnCount++;
+    
+    if (info.hasOwnProperty(nextSpoken)) {
+      info[nextSpoken].turnBefore = info[nextSpoken].turnLastSaid;
+      info[nextSpoken].turnLastSaid = turnCount;
+      info[nextSpoken].timesSpoken++;
+    }
+    else {
+      info[nextSpoken] = { turnBefore: null, turnLastSaid: turnCount, timesSpoken: 1 };
+    }
+
+    if (turnCount === 2020) {
+      console.log(`Part One Answer: ${nextSpoken}`);
+    }
+
+    lastSpoken = nextSpoken;
+  }
+}
