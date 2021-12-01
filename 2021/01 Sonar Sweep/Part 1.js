2fd743d0e0760a87fcd43d86eeb8c3f3fce94ba3
Stephanie McNaught <stephaniemcnaught@gmail.com>
2021-11-30 22:57:03 -0700

    2021 Day 1

--- 2021/01 Sonar Sweep/Part 1.js
+++ 2021/01 Sonar Sweep/Part 1.js
@@ -2,7 +2,6 @@ const formatter = require('../../shared/formatting/format-puzzle-input');
 let report = new formatter.Formatter(__dirname).getArrayOfNumbersByLine();
 let bigger = 0;
 
-
 for (let i = 0; i < report.length; i++) {
   if (i > 0 && report[i] > report[i - 1]) bigger++;
 }
