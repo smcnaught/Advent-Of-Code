d2d2eaacf1df2c99ae065a4213afcbb34a81ec36
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-12-21 21:56:30 -0700

    2022 Day 20

---
+++ 2022/20 Grove Positioning System/Part 2.js
@@ -0,0 +1,17 @@
+const formatter = require('../../shared/formatting/format-puzzle-input');
+const all = [];
+let id = 0;
+new formatter.Formatter(__dirname).getArrayOfStringsByLine().filter(n => n).map(moveBy => all.push({ moveBy: +moveBy * 811589153, id: id++ }));
+const getCircularVal = (i, n) => all[(i % n + n) % n].moveBy;
+function mix() {
+  for (let i = 0; i < id; i++) {
+    const itemInd = all.findIndex(x => x.id === i);
+    const item = all[itemInd].moveBy;
+    all.splice(itemInd, 1);
+    all.splice((item + itemInd) % all.length, 0, { moveBy: item, id: i })
+  }
+}
+for (let i = 0; i < 10; i++) mix();
+const indexOfZero = all.findIndex(el => el.moveBy === 0);
+[valAt1000, valAt2000, valAt3000] = [getCircularVal(indexOfZero + 1000, all.length), getCircularVal(indexOfZero + 2000, all.length), getCircularVal(indexOfZero + 3000, all.length)];
+console.log(`Part One Answer: ${valAt1000 + valAt2000 + valAt3000}`)
