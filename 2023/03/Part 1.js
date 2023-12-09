f39ae6cd985d55d6fcdb39bc0c866dd4c664176d
Stephanie McNaught <stephaniemcnaught@gmail.com>
2023-12-09 14:30:35 -0700

    cleanup

--- 2023/03/Part 1.js
+++ 2023/03/Part 1.js
@@ -2,23 +2,17 @@ const formatter = require('../../shared/formatting/format-puzzle-input');
 let data = new formatter.Formatter(__dirname).get2DArrayOfStrings('');
 let sum = 0
 data.forEach((line, row) => {
-  wholeNum = ''
-  startingPos = null
-  endingPos = null
+  let wholeNum = ''
+  let startingPos = null
 
   line.forEach((d, pos) => {
     if (!isNaN(d)) {
       if (wholeNum === '') startingPos = pos
       wholeNum+=d
-
-      if (pos == line.length - 1) {
-        check(wholeNum, data, startingPos, pos, row)
-        if (wholeNum !== '') wholeNum = ''
-      }
     }
-    else {
+    if (isNaN(d) || pos == line.length - 1) {
       check(wholeNum, data, startingPos, pos, row)
-      if (wholeNum !== '') wholeNum = ''
+      wholeNum = ''
     }
   })
 })
@@ -27,65 +21,25 @@ function check(wholeNum, data, startingPos, pos, row) {
   isPartNum = false
   if (wholeNum !== '') {
     endingPos = (pos - 1) >= 0 ? (pos - 1) : 0
-    if (row !== 0) {
-      // check above
-      rowAbove = data[row - 1]
-      for (let i = startingPos; i <= endingPos; i++) {
-        currentAbove = rowAbove[i]
-        if (currentAbove && isNaN(currentAbove) && currentAbove !== '.') {
-          console.log(`above is part: ${currentAbove}`)
-          isPartNum = true
-        }
-      }
+    ;[itemRight, itemLeft, rowAbove, rowBelow] = [data[row][endingPos+1], data[row][startingPos-1], data[row - 1], data[row + 1]]
 
-      // get diags above
-      for (let i = startingPos - 1; i <= endingPos + 1; i++) {
-        upDiag = rowAbove[i]
-        if (upDiag && isNaN(upDiag) && upDiag !== '.') {
-          console.log(`above diag is part: ${upDiag}`)
-          isPartNum = true
-        }
-      }
-    }
-    if (row !== data.length - 1) {
-      // check below
-      rowBelow = data[row + 1]
-      for (let i = startingPos; i <= endingPos; i++) {
-        currentBelow = rowBelow[i]
-        if (currentBelow && isNaN(currentBelow) && currentBelow !== '.') {
-          console.log(`below is part: ${currentBelow}`)
-          isPartNum = true
-        }
-
-        // get diags below
-        for (let i = startingPos - 1; i <= endingPos + 1; i++) {
-          downDiag = rowBelow[i]
-          if (downDiag && isNaN(downDiag) && downDiag !== '.') {
-            console.log(`down diag is part: ${downDiag}`)
-            isPartNum = true
-          }
+    if ((itemRight && isNaN(itemRight) && itemRight !== '.') || (itemLeft && isNaN(itemLeft) && itemLeft !== '.')) isPartNum = true
+    else {
+      let rowsToCheck = []
+      if (row !== 0) rowsToCheck.push(rowAbove)
+      if (row !== data.length - 1) rowsToCheck.push(rowBelow)
+  
+      for (let i = 0; i < rowsToCheck.length; i++) {
+        currentRow = rowsToCheck[i]
+        for (let j = startingPos - 1; j <= endingPos + 1; j++) {
+          current = currentRow[j]
+          if (current && isNaN(current) && current !== '.') isPartNum = true
         }
       }
     }
 
-    itemRight = data[row][endingPos+1]
-    itemLeft = data[row][startingPos-1]
-
-    if (
-      (itemRight && isNaN(itemRight) && itemRight !== '.') ||
-      (itemLeft && isNaN(itemLeft) && itemLeft !== '.')
-    ) {
-      isPartNum = true
-      console.log(`right or left is part: ${itemLeft}  ${itemRight}`)
-    }
-
-    if (isPartNum) {
-      sum += +wholeNum
-      // console.log(wholeNum)
-    }
+    if (isPartNum) sum += +wholeNum
   }
 }
 
-console.log(sum)
-
-// 540131 = right
+console.log(`Part One Answer: ${sum}`)
